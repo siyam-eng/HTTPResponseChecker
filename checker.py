a7852e468918f493690b544bc47f65c7135e6eb3
Siyam Ahmed <siyamraisa@gmail.com>
2021-01-14 00:17:06 +0600

    updated list of user-agents

--- checker.py
+++ checker.py
@@ -12,7 +12,6 @@ wb = load_workbook(FILE_PATH)
 session = Session()
 HEADERS_LIST = [
     'Mozilla/5.0 (Windows; U; Windows NT 6.1; x64; fr; rv:1.9.2.13) Gecko/20101203 Firebird/3.6.13',
-    'Mozilla/5.0 (compatible, MSIE 11, Windows NT 6.3; Trident/7.0; rv:11.0) like Gecko',
     'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201',
     'Opera/9.80 (X11; Linux i686; Ubuntu/14.10) Presto/2.12.388 Version/12.16',
     'Mozilla/5.0 (Windows NT 5.2; RW; rv:7.0a1) Gecko/20091211 SeaMonkey/9.23a1pre'
